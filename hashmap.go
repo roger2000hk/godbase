a59b8118605be8a96536337b755ea8d6238c143b
fncodr <fncodr@gmail.com>
2016-04-22 09:46:58 +0200

    added hashmap

--- hashmap.go
+++ hashmap.go
@@ -1,9 +1,10 @@
 package godbase
 
-type HashSlots []SkipMap
+type HashSlots []*SkipMap
 type HashFn func (Cmp) uint64
 
 type HashMap struct {
+	alloc *SkipNodeAlloc
 	fn HashFn
 	len int64
 	levels int
@@ -16,26 +17,26 @@ func NewHashMap(fn HashFn, slotCount int, alloc *SkipNodeAlloc, levels int) *Has
 
 func (m *HashMap) Delete(key Cmp, val interface{}) int {
 	i := m.fn(key) % uint64(len(m.slots))
-	res := m.slots[i].Delete(key, val)
-	m.len -= int64(res)
-	return res
+
+	if s := m.slots[i]; s != nil {
+		res := s.Delete(key, val)
+		m.len -= int64(res)
+		return res
+	}
+
+	return 0
 }
 
 func (m *HashMap) Init(fn HashFn, slotCount int, alloc *SkipNodeAlloc, levels int) *HashMap {
+	m.alloc = alloc
 	m.fn = fn
 	m.levels = levels
 	m.slots = make(HashSlots, slotCount)
-
-	for _, s := range m.slots {
-		s.Init(alloc, levels)
-	}
-
 	return m
 }
 
 func (m *HashMap) Insert(key Cmp, val interface{}, allowMulti bool) (interface{}, bool) {
-	i := m.fn(key) % uint64(len(m.slots))
-	res, ok := m.slots[i].Insert(key, val, allowMulti)
+	res, ok := m.getSlot(key).Insert(key, val, allowMulti)
 
 	if ok {
 		m.len++
@@ -46,4 +47,16 @@ func (m *HashMap) Insert(key Cmp, val interface{}, allowMulti bool) (interface{}
 
 func (m *HashMap) Len() int64 {
 	return m.len
+}
+
+func (m *HashMap) getSlot(key Cmp) *SkipMap {
+	i := m.fn(key) % uint64(len(m.slots))
+	s := m.slots[i]
+
+	if s == nil {
+		s = NewSkipMap(m.alloc, m.levels)
+		m.slots[i] = s
+	}
+
+	return s
 }
