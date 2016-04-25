b047d6c4f05033bc1c861b90b401d4e787218b19
fncodr <fncodr@gmail.com>
2016-04-25 14:55:52 +0200

    cleaning

--- maps/test.go
+++ maps/test.go
@@ -1,9 +1,6 @@
 package maps
 
 import (
-	"fmt"
-	"log"
-	"time"
 	"math/rand"
 	"unsafe"
 )
@@ -64,108 +61,4 @@ func randItems(n int) testItems {
 	}
 
 	return res
-}
-
-func PrintTime(start time.Time, msg string, args...interface{}) {
-	elapsed := time.Since(start)
-	log.Printf("%s:\t%s", fmt.Sprintf(msg, args...), elapsed)
-}
-
-func (m *ESkip) testDelete(start, end Iter, key Cmp, val interface{}) (Iter, int) {
-	if val != nil {
-		val = &val.(*testItem).skipNode
-	}
-
-	res, cnt := m.Delete(start, end, key, val)
-	return res, cnt
-}
-
-func (m *SkipHash) testDelete(start, end Iter, key Cmp, val interface{}) (Iter, int) {
-	res, cnt := m.Delete(start, end, key, val)
-	return res, cnt
-}
-
-func (m *ESkipHash) testDelete(start, end Iter, key Cmp, val interface{}) (Iter, int) {
-	if val != nil {
-		val = &val.(*testItem).skipNode
-	}
-	
-	res, cnt := m.Delete(start, end, key, val)
-	return res, cnt
-}
-
-func (m Map) testDelete(start, end Iter, key Cmp, val interface{}) (Iter, int) {
-	res, cnt := m.Delete(start, end, key, val)
-	return res, cnt
-}
-
-func (m *Skip) testDelete(start, end Iter, key Cmp, val interface{}) (Iter, int) {
-	res, cnt := m.Delete(start, end, key, val)
-	return res, cnt
-}
-
-func (m *ESkip) testFind(start Iter, key Cmp, val interface{}) (Iter, bool) {
-	if val != nil {
-		val = &val.(*testItem).skipNode
-	}
-
-	res, ok := m.Find(start, key, val)
-	return res, ok
-}
-
-func (m *SkipHash) testFind(start Iter, key Cmp, val interface{}) (Iter, bool) {
-	res, ok := m.Find(start, key, val)
-	return res, ok
-}
-
-func (m *ESkipHash) testFind(start Iter, key Cmp, val interface{}) (Iter, bool) {
-	if val != nil {
-		val = &val.(*testItem).skipNode
-	}
-	
-	res, ok := m.Find(start, key, val)
-	return res, ok
-}
-
-func (m Map) testFind(start Iter, key Cmp, val interface{}) (Iter, bool) {
-	res, ok := m.Find(start, key, val)
-	return res, ok
-}
-
-func (m *Skip) testFind(start Iter, key Cmp, val interface{}) (Iter, bool) {
-	res, ok := m.Find(start, key, val)
-	return res, ok
-}
-
-func (m *ESkip) testInsert(start Iter, key Cmp, val interface{}, allowMulti bool) (Iter, bool) {
-	if val != nil {
-		val = &val.(*testItem).skipNode
-	}
-
-	res, ok := m.Insert(start, key, val, allowMulti)
-	return res, ok
-}
-
-func (m *SkipHash) testInsert(start Iter, key Cmp, val interface{}, allowMulti bool) (Iter, bool) {
-	res, ok := m.Insert(start, key, val, allowMulti)
-	return res, ok
-}
-
-func (m *ESkipHash) testInsert(start Iter, key Cmp, val interface{}, allowMulti bool) (Iter, bool) {
-	if val != nil {
-		val = &val.(*testItem).skipNode
-	}
-
-	res, ok := m.Insert(start, key, val, allowMulti)
-	return res, ok
-}
-
-func (m Map) testInsert(start Iter, key Cmp, val interface{}, allowMulti bool) (Iter, bool) {
-	res, ok := m.Insert(start, key, val, allowMulti)
-	return res, ok
-}
-
-func (m *Skip) testInsert(start Iter, key Cmp, val interface{}, allowMulti bool) (Iter, bool) {
-	res, ok := m.Insert(start, key, val, allowMulti)
-	return res, ok
 }
