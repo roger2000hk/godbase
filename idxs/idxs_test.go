240a414d576b302b9aea7f65eccfe323de2993a1
fncodr <fncodr@gmail.com>
2016-04-27 14:45:54 +0200

    idx tests

--- idxs/idxs_test.go
+++ idxs/idxs_test.go
@@ -5,7 +5,11 @@ import (
 )
 
 func TestKeyEq(t *testing.T) {
-	gen := func(k1, k2 string) interface{} {
+	// array equality is pretty cool
+	// as long as the legth can be determined at compile time,
+	// any comparable types can be packed up and compared in one op
+
+	gen := func(k1, k2 interface{}) interface{} {
 		return [2]interface{}{k1, k2}
 	}
 	
@@ -16,4 +20,13 @@ func TestKeyEq(t *testing.T) {
 	if  l, r := gen("abc", "def"), gen("abc", "ghi"); l == r {
 		t.Errorf("equal")
 	}
+
+	if  l, r := gen(1, 2), gen(1, 2); l != r {
+		t.Errorf("not equal")
+	}
+
+	if  l, r := gen("abc", "def"), gen(1, 2); l == r {
+		t.Errorf("equal")
+	}
+
 }
