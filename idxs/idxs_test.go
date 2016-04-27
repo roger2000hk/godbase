f61f746b780c24edf6b04707d4c0c93a1670929f
fncodr <fncodr@gmail.com>
2016-04-27 14:40:48 +0200

    idx tests

--- idxs/idxs_test.go
+++ idxs/idxs_test.go
@@ -5,11 +5,15 @@ import (
 )
 
 func TestKeyEq(t *testing.T) {
-	gen := func() interface{} {
-		return [2]interface{}{"abc", "def"}
+	gen := func(k1, k2 string) interface{} {
+		return [2]interface{}{k1, k2}
 	}
 	
-	if  l, r := gen(), gen(); l != r {
+	if  l, r := gen("abc", "def"), gen("abc", "def"); l != r {
 		t.Errorf("not equal")
+	}
+
+	if  l, r := gen("abc", "def"), gen("abc", "ghi"); l == r {
+		t.Errorf("equal")
 	}
 }
