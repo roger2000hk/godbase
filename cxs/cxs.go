8c6fa2f260cfe0419a08151a17d59fcfe337b6e9
fncodr <fncodr@gmail.com>
2016-05-04 02:49:48 +0200

    removed cx mapalloc

--- cxs/cxs.go
+++ cxs/cxs.go
@@ -1,23 +1,11 @@
 package cxs
 
 import (
-	"github.com/fncodr/godbase"
-	"github.com/fncodr/godbase/maps"
 )
 
 type Basic struct {
-	mapAlloc maps.SlabAlloc
 }
 
-func New(mss int) godbase.Cx {
-	return new(Basic).Init(mss)
-}
-
-func (self *Basic) Init(mss int) *Basic {
-	self.mapAlloc.Init(mss)
+func (self *Basic) Init() *Basic {
 	return self
-}
-
-func (self *Basic) MapAlloc() *maps.SlabAlloc {
-	return &self.mapAlloc
 }
